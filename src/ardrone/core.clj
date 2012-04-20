9690d09348312981f39fbde9caa6b566d8420ff7
Burke Libbey <burke@burkelibbey.org>
2012-04-20 13:27:21 -0400

    Initial

---
+++ src/ardrone/core.clj
@@ -0,0 +1,1 @@
+(ns ardrone.core)
---
+++ test/ardrone/test/core.clj
@@ -0,0 +1,6 @@
+(ns ardrone.test.core
+  (:use [ardrone.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
